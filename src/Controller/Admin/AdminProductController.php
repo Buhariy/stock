b034a327878f299cf4ec3e75b9edc2bb9e2582e4
Buhariy <buhari.yavuz@gmail.com>
2022-07-05 10:52:28 +0200

    debog list products

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -5,11 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminProductController extends AbstractController
 {
     /**
-     * @Route("/admin/listproducts", name="admin_list_product")
+     * @Route("admin/listproducts", name="admin_list_product")
      */
     public function list_products(ProductRepository $productRepository)
     {
