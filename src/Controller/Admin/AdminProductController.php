e8c4054c87b09989cbaef91bcf02c549360570f6
Buhariy <buhari.yavuz@gmail.com>
2022-07-05 13:57:49 +0200

    add form product

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminProductController extends AbstractController
 {
@@ -17,5 +21,40 @@ class AdminProductController extends AbstractController
         $products = $productRepository->findAll();
 
         return $this->render("admin/list_products.html.twig", ["products" => $products]);
+    }
+
+    /**
+     * @Route("admin/createproduct", name="admin_create_product")
+     */
+    public function createProduct(Request $request, EntityManagerInterface $entityManagerInterface, SluggerInterface $sluggerInterface)
+    {
+        $product = new Product();
+
+        $productForm = $this->createForm(ProductType::class, $product);
+
+        $productForm->handleRequest($request);
+
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            $productFile = $productForm->get('img')->getData();
+
+            if ($productFile) {
+                $originaleFileName = pathinfo($productFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFileName = $sluggerInterface->slug($originaleFileName);
+                $newFileName = $safeFileName . '-' . uniqid() . '.' . $productFile->guessExtension();
+
+                $productFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFileName
+                );
+
+                $product->setImg($newFileName);
+            }
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute("admin_list_product");
+        }
+
+        return $this->render("admin/form_product.html.twig", ["productForm" => $productForm->createView()]);
     }
 }
